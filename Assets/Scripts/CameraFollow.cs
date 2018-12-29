ac80896553aa5b7e626ac360bcb0b919cbd26425
NIk <nikkowalski@hotmail.com>
2018-12-29 04:58:04 -0600

    Add files via upload

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,148 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+
+    public Controller2D target;
+    public Vector3 focusAreaSize;
+    public float verticalOffset;
+    public float cameraDistanceZ = -5;
+    public float lookAheadDistanceX;
+    public float lookSmoothTimeX;
+    public float verticalSmoothTime;
+
+    float currentLookAheadX;
+    float targetLookAheadX;
+    float lookAheadDirectionX;
+    float smoothLookVelocityX;
+    float smoothVelocityY;
+
+    bool lookAheadStopped;
+
+    FocusArea focusArea;
+
+    void Start()
+    {
+        focusArea = new FocusArea(target.collider.bounds, focusAreaSize);
+
+
+    }
+
+
+    void LateUpdate()
+    {
+        focusArea.Update(target.collider.bounds);
+        Vector3 focusPosition = focusArea.center + Vector3.up * verticalOffset;
+
+        if(focusArea.velocity.x != 0)
+        {
+            lookAheadDirectionX = Mathf.Sign(focusArea.velocity.x);
+
+            if(Mathf.Sign(target.playerInput.x) == Mathf.Sign(focusArea.velocity.x) && target.playerInput.x != 0)
+            {
+                lookAheadStopped = false;
+                 targetLookAheadX = lookAheadDirectionX * lookAheadDistanceX;
+
+
+            }
+            else
+            {
+                if(!lookAheadStopped)
+                {
+                    lookAheadStopped = true;
+                    targetLookAheadX = currentLookAheadX + (lookAheadDirectionX * lookAheadDistanceX - currentLookAheadX) / 4f;
+
+
+
+                }
+
+
+            }
+        }
+
+
+        currentLookAheadX = Mathf.SmoothDamp(currentLookAheadX, targetLookAheadX, ref smoothLookVelocityX, lookSmoothTimeX);
+
+        focusPosition.y = Mathf.SmoothDamp(transform.position.y, focusPosition.y, ref smoothVelocityY, verticalSmoothTime);
+
+        focusPosition += Vector3.right * currentLookAheadX;
+
+        transform.position = (Vector3)focusPosition + Vector3.forward * cameraDistanceZ;
+    }
+
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = new Color(0, 0, 1, 0.25f);
+        Gizmos.DrawCube(focusArea.center, focusAreaSize);
+
+
+    }
+
+
+
+    struct FocusArea
+    {
+        public Vector3 center;
+        public Vector3 velocity;
+        float left, right;
+        float top, bottom;
+
+        public FocusArea(Bounds targetBounds, Vector3 size)
+        {
+           
+
+            left = targetBounds.center.x - size.x / 2;
+            right = targetBounds.center.x + size.x / 2;
+            bottom = targetBounds.min.y;
+            top = targetBounds.min.y + size.y;
+
+            velocity = Vector3.zero;
+            center = new Vector3((left+right)/2, (top+bottom)/2);
+
+        }
+
+        public void Update(Bounds targetBounds)
+        {
+            float shiftX = 0;
+            if(targetBounds.min.x < left)
+            {
+                shiftX = targetBounds.min.x - left;
+
+
+            }
+            else if(targetBounds.max.x > right)
+            {
+                shiftX = targetBounds.max.x - right;
+
+
+            }
+            left += shiftX;
+            right += shiftX;
+
+            float shiftY = 0;
+            if(targetBounds.min.y < bottom)
+            {
+                shiftY = targetBounds.min.y - bottom;
+
+
+            }else if(targetBounds.max.y > top)
+            {
+
+                shiftY = targetBounds.max.y - top;
+
+            }
+            top += shiftY;
+            bottom += shiftY;
+            center = new Vector3((left + right) / 2, (top + bottom) / 2, 0);
+            velocity = new Vector3(shiftX, shiftY);
+
+        }
+
+
+    }
+
+
+}
---
+++ Assets/Scripts/PlatformController.cs
@@ -0,0 +1,291 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformController : RaycastController {
+
+
+    
+    public LayerMask passengerMask;
+
+    public Vector3[] localWaypoints;
+    public Vector3[] globalWaypoints;
+
+    public bool cyclic;
+
+    public float speed;
+    public float waitTime;
+    [Range(0, 3)]
+    public float easeAmount;
+
+    int fromWaypointIndex;
+
+    float percentBetweenWaypoints;
+    float nextMoveTime;
+
+    //List<PassengerMovement> passengerMovement = new List<PassengerMovement>();
+    List<PassengerMovement> passengerMovement;
+    Dictionary<Transform, Controller2D> passengerDictionary = new Dictionary<Transform, Controller2D>();
+
+
+    
+    // Use this for initialization
+    public override void Start () {
+        base.Start();
+
+        globalWaypoints = new Vector3[localWaypoints.Length];
+        for(int i=0; i < localWaypoints.Length; i++)
+        {
+            globalWaypoints[i] = localWaypoints[i] + transform.position;
+
+
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        UpdateRaycastOrigins();
+
+        Vector3 velocity = CalculatePlatformMovement();
+        CalculatePassengerMovement(velocity);
+        MovePassengers(true);
+        transform.Translate(velocity);
+        MovePassengers(false);
+    }
+
+    float Ease(float x)
+    {
+        float a = easeAmount + 1;
+
+        return Mathf.Pow(x,a)/(Mathf.Pow(x,a) + Mathf.Pow(1-x, a));
+    }
+
+    
+    Vector3 CalculatePlatformMovement()
+    {
+        if(Time.time < nextMoveTime)
+        {
+
+
+            return Vector3.zero;
+        }
+
+        
+            fromWaypointIndex %= globalWaypoints.Length;
+
+            int toWaypointIndex = (fromWaypointIndex + 1) % globalWaypoints.Length;
+
+            float distanceBetweenWaypoints = Vector3.Distance(globalWaypoints[fromWaypointIndex], globalWaypoints[toWaypointIndex]);
+            percentBetweenWaypoints += Time.deltaTime * speed / distanceBetweenWaypoints;
+            percentBetweenWaypoints = Mathf.Clamp01(percentBetweenWaypoints);
+            float easedPercentBetweenWaypoints = Ease(percentBetweenWaypoints);
+
+            Vector3 newPos = Vector3.Lerp(globalWaypoints[fromWaypointIndex], globalWaypoints[toWaypointIndex], easedPercentBetweenWaypoints);
+
+            if (percentBetweenWaypoints >= 1)
+            {
+                percentBetweenWaypoints = 0;
+                fromWaypointIndex++;
+                if (!cyclic)
+                {
+                    if (fromWaypointIndex >= globalWaypoints.Length - 1)
+                    {
+                        fromWaypointIndex = 0;
+                        System.Array.Reverse(globalWaypoints);
+
+                    }
+                }
+                nextMoveTime = Time.time + waitTime;
+            }
+
+            return newPos - transform.position;
+        
+    }
+
+
+    void MovePassengers(bool beforeMovePlatform)
+    {
+        
+            foreach (PassengerMovement passenger in passengerMovement)
+            {
+
+
+                if (!passengerDictionary.ContainsKey(passenger.transform))
+                {
+                    passengerDictionary.Add(passenger.transform, passenger.transform.GetComponent<Controller2D>());
+
+
+                }
+                if (passenger.moveBeforePlatform == beforeMovePlatform)
+                {
+                    passengerDictionary[passenger.transform].Move(passenger.velocity, passenger.standingOnPlatform);
+
+
+                }
+
+
+            }
+        
+        
+        
+
+
+    }
+
+
+    void CalculatePassengerMovement(Vector3 velocity)
+    {
+        HashSet<Transform> movedPassengers = new HashSet<Transform>();
+        passengerMovement = new List<PassengerMovement>();
+
+        float directionX = Mathf.Sign(velocity.x);
+        float directionY = Mathf.Sign(velocity.y);
+
+        // Vertically Moving Platform
+        if (velocity.y != 0)
+        {
+            float rayLength = (Mathf.Abs(velocity.y) + skinWidth);
+
+            for (int i = 0; i < verticalRayCount; i++)
+            {
+                Vector3 rayOrigin = (directionY == -1) ? raycastOrigins.bottomLeft : raycastOrigins.topLeft;
+                rayOrigin += Vector3.right * (verticalRaySpacing * i);
+                RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector3.up * directionY, rayLength, passengerMask);
+                //Debug.DrawRay(rayOrigin, Vector2.up * directionY, Color.blue, Mathf.Infinity);
+
+                if (hit && hit.distance != 0)
+                {
+                    //print("hit ");
+                    if (!movedPassengers.Contains(hit.transform))
+                    {
+                        movedPassengers.Add(hit.transform);
+                        float pushX = (directionY == 1) ? velocity.x : 0;
+                        float pushY = velocity.y - (hit.distance - skinWidth) * directionY;
+
+                        passengerMovement.Add(new PassengerMovement(hit.transform, new Vector3(pushX, pushY), directionY == 1, true));
+                    }
+
+                }
+
+            }
+
+        }
+
+
+        // Horizontally Moving Paltform
+        if (velocity.x != 0)
+        {
+            float rayLength = Mathf.Abs(velocity.x) + skinWidth;
+
+            for (int i = 0; i < horizontalRayCount; i++)
+            {
+                Vector3 rayOrigin = (directionX == -1) ? raycastOrigins.bottomLeft : raycastOrigins.bottomRight;
+                rayOrigin += Vector3.up * (horizontalRaySpacing * i);
+                RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector3.right * directionX, rayLength, passengerMask);
+                //Debug.DrawRay(rayOrigin, Vector3.right * directionX, Color.blue);
+                if (hit && hit.distance != 0)
+                {
+                    //print("hit ");
+                    if (!movedPassengers.Contains(hit.transform))
+                    {
+                        movedPassengers.Add(hit.transform);
+                        float pushX = velocity.x - (hit.distance - skinWidth) * directionX;
+                        float pushY = -skinWidth;
+
+                        passengerMovement.Add(new PassengerMovement(hit.transform, new Vector3(pushX, pushY), false, true));
+                    }
+
+                }
+
+            }
+
+
+
+        }
+
+
+        // Passenger on top of horizontally or downward moving platform
+        if((directionY == -1 )|| ((velocity.y == 0) && (velocity.x != 0)))
+        {
+            float rayLength = skinWidth * 2;
+
+            for (int i = 0; i < verticalRayCount; i++)
+            {
+                Vector3 rayOrigin = raycastOrigins.topLeft + Vector3.right * (verticalRaySpacing * i);
+                RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector3.up, rayLength, passengerMask);
+                //Debug.DrawRay(rayOrigin, Vector3.up * directionY, Color.blue, Mathf.Infinity);
+                if (hit && hit.distance != 0)
+                {
+                    //print("hit ");
+                    if (!movedPassengers.Contains(hit.transform))
+                    {
+                        movedPassengers.Add(hit.transform);
+                        float pushX = velocity.x;
+                        float pushY = velocity.y;
+
+                        passengerMovement.Add(new PassengerMovement(hit.transform, new Vector3(pushX, pushY), false, false));
+                    }
+
+                }
+
+            }
+
+
+
+
+        }
+
+
+
+
+    }
+
+
+    struct PassengerMovement
+    {
+        public Transform transform;
+        public Vector3 velocity;
+        public bool standingOnPlatform;
+        public bool moveBeforePlatform;
+
+        public PassengerMovement(Transform _transform, Vector3 _velocity, bool _standingOnPlatform, bool _moveBeforePlatform)
+        {
+            transform = _transform;
+            velocity = _velocity;
+            standingOnPlatform = _standingOnPlatform;
+            moveBeforePlatform = _moveBeforePlatform;
+
+
+
+        }
+
+
+    }
+
+    void OnDrawGizmos()
+    {
+        if(localWaypoints != null)
+        {
+            Gizmos.color = Color.red;
+            float size = 0.9f;
+            float cubeArea = 0.7f;
+
+            for(int i=0; i < localWaypoints.Length; i++)
+            {
+                Vector3 globalWaypointPos = (Application.isPlaying)?globalWaypoints[i] : localWaypoints[i] + transform.position;
+                Gizmos.DrawLine(globalWaypointPos - Vector3.up * size, globalWaypointPos + Vector3.up * size);
+                Gizmos.DrawLine(globalWaypointPos - Vector3.left * size, globalWaypointPos + Vector3.left * size);
+                Gizmos.color = new Color(1, 1, 0, 0.25f);
+                Gizmos.DrawCube(globalWaypointPos, new Vector3(cubeArea , cubeArea, -1));
+                Gizmos.color = Color.red;
+
+            }
+
+        }
+
+
+    }
+
+
+}
